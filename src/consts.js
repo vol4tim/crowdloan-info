3807bca04e30500a83b29a92fee916709c08789b
vol4tim <vol4tim@gmail.com>
2021-06-28 11:35:47 +0400

    config host port

--- src/consts.js
+++ src/consts.js
@@ -5,15 +5,15 @@ export default {
   DEBUG: process.env.DEBUG
     ? process.env.DEBUG.trim().toLowerCase() === "true"
     : false,
-  HOST: process.env.HOST || "127.0.0.1",
-  PORT: process.env.PORT || "3012",
+  HOST: process.env.HOST || config.server.host,
+  PORT: process.env.PORT || config.server.port,
   SSL_ENABLE: process.env.SSL_ENABLE
     ? process.env.SSL_ENABLE.trim().toLowerCase() === "true"
     : false,
   SSL: {
     key: process.env.SSL_KEY || "",
-    cer: process.env.SSL_CER || ""
+    cer: process.env.SSL_CER || "",
   },
   PATH_DB: path.join(__dirname, "/../files/database.sqlite"),
-  ...config
+  ...config,
 };
